c2a8fd001eabbf2cfa724238058114768284762b
Carlos Augusto <carlos.brandao@etipi.pi.gov.br>
2024-04-29 22:07:30 -0300

    chore

---
+++ src/main/java/com/example/dscommercejavabackend/DsCommerceJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dscommercejavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCommerceJavaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCommerceJavaBackendApplication.class, args);
+	}
+
+}
